59af4865c74940d2f198dfdf709be5ed4b30ad75
PPhatDev <leatsophat02@gmail.com>
2024-12-16 16:38:06 +0700

    📂: Created Image Profile SEO Header

---
+++ src/utils/seo/profile.ts
@@ -0,0 +1,41 @@
+const domain = window.location.origin ?? ""
+const identifier = import.meta.env.VITE_APP_ID ?? ""
+const name = import.meta.env.VITE_APP_NAME ?? ""
+const description = import.meta.env.VITE_APP_DESCRIPTION ?? ""
+const profilePath = import.meta.env.VITE_APP_PROFILE_PATH ?? ""
+
+export const ProfilePage = (
+    `<script type="application/ld+json">{
+        "@context": "https://schema.org",
+        "@type": "ProfilePage",
+        "dateCreated": "2024-12-23T12:34:00-05:00",
+        "dateModified": "2024-12-26T14:53:00-05:00",
+        "mainEntity": {
+            "@type": "Person",
+            "name": "${name}",
+            "alternateName": "${name.replace(/\s+/g, '').toLowerCase()}",
+            "identifier": "${identifier}",
+            "interactionStatistic": [
+                {
+                    "@type": "InteractionCounter",
+                    "interactionType": "https://schema.org/FollowAction",
+                    "userInteractionCount": 1
+                },{
+                    "@type": "InteractionCounter",
+                    "interactionType": "https://schema.org/LikeAction",
+                    "userInteractionCount": 5
+                }
+            ],
+            "agentInteractionStatistic": {
+                "@type": "InteractionCounter",
+                "interactionType": "https://schema.org/WriteAction",
+                "userInteractionCount": 2346
+            },
+            "description": "${description}",
+            "image": "${domain}/${profilePath}",
+            "sameAs": [
+                "${domain}",
+            ]
+        }
+    } </script>`
+) 
