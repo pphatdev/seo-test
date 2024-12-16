3a25d80087694de2b0de360139371197ea67e31b
PPhatDev <leatsophat02@gmail.com>
2024-12-16 16:39:14 +0700

    🗃️: Merged SEO Header

---
+++ src/utils/seo/index.ts
@@ -0,0 +1,53 @@
+import { type ImageMetaData, imageMetaData as setMetaData } from "./image-metadata";
+
+interface MetaTag {
+    name: string;
+    content: string;
+}
+
+interface OpenGraphTag {
+    property: string;
+    content: string;
+}
+
+const generateMetaTags = (
+    tags: MetaTag[] = []
+): string => {
+    return tags.map(tag => `<meta name="${tag.name}" content="${tag.content}">`).join('\n');
+};
+
+const generateOpenGraphTags = (
+    tags: OpenGraphTag[] = []
+): string => {
+    return tags.map(tag => `<meta property="${tag.property}" content="${tag.content}">`).join('\n');
+};
+
+export const setSEO = ({
+    title,
+    description,
+    metaTags,
+    openGraphTags,
+    profilePage,
+    imageMetaData
+}: {
+    title: string;
+    description: string;
+    metaTags?: MetaTag[];
+    openGraphTags?: OpenGraphTag[],
+    profilePage?: string,
+    imageMetaData?: ImageMetaData
+}): string => {
+    const metaTagString = generateMetaTags(metaTags);
+    const openGraphTagString = generateOpenGraphTags(openGraphTags);
+    
+    const head =  `
+        <title>${title}</title>
+        <meta name="description" content="${description}">
+        ${metaTagString ?? ""}
+        ${openGraphTagString ?? ""}
+        ${profilePage ?? ""}
+        ${imageMetaData && setMetaData(imageMetaData)}
+    `;
+
+    return document.head.innerHTML += head
+};
