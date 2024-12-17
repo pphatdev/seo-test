7f701193ef8219653854438953f3aef04f8efa2d
PPhatDev <leatsophat02@gmail.com>
2024-12-17 17:59:50 +0700

    🎨: Example Setup Home page

--- src/pages/home/page.ts
+++ src/pages/home/page.ts
@@ -1,22 +1,55 @@
-import { setSEO } from "../../utils/seo/index";
+import { Bg as Background } from "@components/bg";
+import { renderAsElement as oscillator } from "@components/canvas";
+import { setSEO } from "@utils/seo/index";
+import { ProfilePage } from "@utils/seo/profile";
+import heading from "@templates/header.stub?raw";
 
 const meta = {
     title: 'Home Page Title',
     description: 'Description for Home Page',
-    // metaTags: [],
-    // openGraphTags: []
-
-    imageMetaData: {
-        url: "path/to/image.jpg",
-        title: "Description of the image",
-    }
+    metaTags: [
+        {
+            name: "description",
+            content: `Author: ${import.meta.env.VITE_APP_NAME}`
+        },
+        {
+            name: "robots",
+            content: "noindex, nofollow"
+        }
+    ],
+    profilePage: ProfilePage()
 }
+
+const menu = [
+    {
+        title: "Home",
+        url: "/",
+    },
+    {
+        title: "Services",
+        url: "/",
+    },
+    {
+        title: "Our Project",
+        url: "/",
+    },
+    {
+        title: "About us",
+        url: "/",
+    },
+]
 
 
 const Home = () => {
     setSEO(meta);
-    
-    return `<h1>Home Page</h1>`;
+    const template  = new Function('menu', `return \`${heading}\``);
+    const header    = template(menu);
+
+    return Background(`
+        ${oscillator}
+        ${header}
+        <div class="h-[calc(100vh_-58px)]"></div>
+    `);
 };
 
-export default Home; 
+export default Home;
