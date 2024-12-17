57c2e48203f757a62ac62478fc9f9f3f242c73a1
PPhatDev <leatsophat02@gmail.com>
2024-12-17 17:49:51 +0700

    🆕: VITE resolve

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@styles": path.resolve(__dirname, "./src/styles"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+            "@templates": path.resolve(__dirname, "./src/templates"),
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }
+}
