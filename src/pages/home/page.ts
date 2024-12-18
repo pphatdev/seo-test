899e95f9cf275d40c09d16b046ca45e93f4d3eb8
PPhatDev <leatsophat02@gmail.com>
2024-12-18 16:36:58 +0700

    🌈: Updated home page

--- src/pages/home/page.ts
+++ src/pages/home/page.ts
@@ -1,54 +1,16 @@
 import { Bg as Background } from "@components/bg";
 import { renderAsElement as oscillator } from "@components/canvas";
-import { setSEO } from "@utils/seo/index";
-import { ProfilePage } from "@utils/seo/profile";
-import heading from "@templates/header.stub?raw";
-
-const meta = {
-    title: 'Home Page Title',
-    description: 'Description for Home Page',
-    metaTags: [
-        {
-            name: "description",
-            content: `Author: ${import.meta.env.VITE_APP_NAME}`
-        },
-        {
-            name: "robots",
-            content: "noindex, nofollow"
-        }
-    ],
-    profilePage: ProfilePage()
-}
-
-const menu = [
-    {
-        title: "Home",
-        url: "/",
-    },
-    {
-        title: "Services",
-        url: "/",
-    },
-    {
-        title: "Our Project",
-        url: "/",
-    },
-    {
-        title: "About us",
-        url: "/",
-    },
-]
+import { headerSection } from "@templates/header";
+import homeSEO from "@pages/home/seo";
+import { heroSection } from "@pages/home/hero";
 
 
 const Home = () => {
-    setSEO(meta);
-    const template  = new Function('menu', `return \`${heading}\``);
-    const header    = template(menu);
-
+    homeSEO();
     return Background(`
         ${oscillator}
-        ${header}
-        <div class="h-[calc(100vh_-58px)]"></div>
+        ${headerSection}
+        ${heroSection}
     `);
 };
 
