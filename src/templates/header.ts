bd5c2ed994caaae17f2ee89a09aa1de39906cadb
PPhatDev <leatsophat02@gmail.com>
2024-12-18 16:33:10 +0700

    ⚙️: Updated header stub to ts file

---
+++ src/templates/header.ts
@@ -0,0 +1,26 @@
+import { menu } from "@/components/menu"
+import { themeSwitcherButton } from "@/components/theme"
+
+export const headerSection = (()=> {
+    return(
+        `<header class="flex justify-between items-center w-full px-2 md:px-5 inset-x-0 sticky pt-4 z-50 max-w-6xl mx-auto">
+            <div class="px-5 flex py-2 rounded-full bg-foreground/5 backdrop-blur-xl text-foreground">
+                Logo
+            </div>
+            <nav class="mx-auto hidden sm:flex w-full max-w-md px-5 py-2 rounded-full bg-foreground/5 backdrop-blur-xl">
+                <ul class="inline-flex gap-2 justify-center w-full">
+                    ${menu.map(item=> {
+                        return (
+                            `<li class="font-normal rounded-full p-2 px-4 ${item.isActive ? 'text-primary' : 'text-foreground/80 hover:text-foreground'}">
+                                <a href="${item.url}" class="font-medium">${item.title}</a>
+                            </li>`
+                        )
+                    }).join("")}
+                </ul>
+            </nav>
+            <div class="px-2 py-2 flex rounded-full items-center justify-center bg-foreground/5 backdrop-blur-xl">
+                ${themeSwitcherButton}
+            </div>
+        </header>`
+    )
+})()
