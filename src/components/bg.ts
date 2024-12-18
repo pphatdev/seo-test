c19247d2d3f8bd67452d374e1c2407817928f1c5
PPhatDev <leatsophat02@gmail.com>
2024-12-18 16:35:03 +0700

    🌈: Applied background spaceElevator animation

--- src/components/bg.ts
+++ src/components/bg.ts
@@ -1,12 +1,16 @@
 import { BgEffectLeft, BgEffectRight } from "./bg-effect"
+import { spaceElevatorSVG } from "./updown"
 
 export const Bg = (element: string = "") => {
     return(`
-        <div class="overflow-hidden w-full relative">
-            <div class="absolute inset-y-0 left-0 -translate-x-1/2">
+        <div class="overflow-hidden min-h-screen w-full relative">
+            <div class="absolute inset-0 pointer-events-none">
+                ${spaceElevatorSVG('h-screen')}
+            </div>
+            <div class="absolute inset-y-0 left-0 -translate-x-1/2 pointer-events-none">
                 ${BgEffectRight}
             </div>
-            <div class="absolute inset-y-0 right-0 translate-x-1/2">
+            <div class="absolute inset-y-0 right-0 translate-x-1/2 pointer-events-none">
                 ${BgEffectLeft}
             </div>
             ${element}
