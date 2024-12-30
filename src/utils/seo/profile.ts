bb49ceae4eecfd77df098c0c45a61da8aa18289c
PPhatDev <leatsophat02@gmail.com>
2024-12-30 16:07:56 +0700

    🤗: Updated Host for ProfilePage

--- src/utils/seo/profile.ts
+++ src/utils/seo/profile.ts
@@ -9,38 +9,37 @@ const profilePath = import.meta.env.VITE_APP_PROFILE_PATH ?? ""
 
 export const ProfilePage = () => minify(
     `<script type="application/ld+json">
-    {
-        "@context": "https://schema.org",
-        "@type": "ProfilePage",
-        "dateCreated": "2024-12-23T12:34:00-05:00",
-        "dateModified": "2024-12-26T14:53:00-05:00",
-        "mainEntity": {
-            "@type": "Person",
-            "name": "${name}",
-            "alternateName": "${name.replace(/\s+/g, '').toLowerCase()}",
-            "identifier": "${identifier}",
-            "interactionStatistic": [
-                {
+        {
+            "@context": "https://schema.org",
+            "@type": "ProfilePage",
+            "dateCreated": "2024-12-23T12:34:00-05:00",
+            "dateModified": "2024-12-26T14:53:00-05:00",
+            "mainEntity": {
+                "@type": "Person",
+                "name": "${name}",
+                "alternateName": "${name.replace(/\s+/g, '').toLowerCase()}",
+                "identifier": "${identifier}",
+                "interactionStatistic": [
+                    {
+                        "@type": "InteractionCounter",
+                        "interactionType": "https://schema.org/FollowAction",
+                        "userInteractionCount": 1
+                    },
+                    {
+                        "@type": "InteractionCounter",
+                        "interactionType": "https://schema.org/LikeAction",
+                        "userInteractionCount": 5
+                    }
+                ],
+                "agentInteractionStatistic": {
                     "@type": "InteractionCounter",
-                    "interactionType": "https://schema.org/FollowAction",
-                    "userInteractionCount": 1
+                    "interactionType": "https://schema.org/WriteAction",
+                    "userInteractionCount": 2346
                 },
-                {
-                    "@type": "InteractionCounter",
-                    "interactionType": "https://schema.org/LikeAction",
-                    "userInteractionCount": 5
-                }
-            ],
-            "agentInteractionStatistic": {
-                "@type": "InteractionCounter",
-                "interactionType": "https://schema.org/WriteAction",
-                "userInteractionCount": 2346
-            },
-            "description": "${description}",
-            "image": "${domain}/${profilePath}",
-            "sameAs": ${JSON.stringify(sameAs)}
+                "description": "${description}",
+                "image": "${domain}/${profilePath}",
+                "sameAs": ${JSON.stringify(sameAs)}
+            }
         }
-    } </script>`
+    </script>`
 )
-
-console.log(ProfilePage());
