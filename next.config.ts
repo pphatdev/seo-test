d57b66fac0c4b63a10064b960c61e2260512a939
pphatdev <leatsophat02@gmail.com>
2025-01-19 20:10:35 +0700

    ⚙️: Update structure:

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/seo-test",
+    // basePath: "/seo-test",
     output: "export",
-    reactStrictMode: true,
 };
 
 module.exports = nextConfig;
