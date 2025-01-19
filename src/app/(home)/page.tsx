87088529855359b1342c55959865710cbfb175ae
pphatdev <leatsophat02@gmail.com>
2025-01-19 18:49:15 +0700

    🌈: Updated home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "./sections/hero";
+
+export default function Home() {
+    return (
+        <div className="w-full mx-auto min-h-screen overflow-y-auto">
+            <Hero />
+            {/* <Collision /> */}
+        </div>
+    );
+}
