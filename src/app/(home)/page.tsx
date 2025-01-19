e6e0d489db7df0ccfd43a6e95d9acb4fd9182986
pphatdev <leatsophat02@gmail.com>
2025-01-19 22:41:45 +0700

    🛩️: Turned on SEO

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,27 @@
 import React from "react";
 import Hero from "./sections/hero";
+import { Metadata } from "next";
+import { appDescriptions, appName } from "@lib/data";
+
+export const metadata: Metadata = {
+    title: appName,
+    description: appDescriptions,
+    authors: [{
+        url: "https://new-pphat.netlify.app",
+        name: "Leat Sophat",
+    }],
+    generator: "PPhat Dev",
+    openGraph: {
+        type: "profile",
+        url: "https://new-pphat.netlify.app",
+        title: appName,
+        description: appDescriptions,
+        siteName: appName,
+        images: [{
+            url: "https://new-pphat.netlify.app/assets/avatars/hero.webp",
+        }],
+    }
+};
 
 export default function Home() {
     return (
