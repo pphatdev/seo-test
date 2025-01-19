294c730677e5826288421a59f952b002d3e771fb
pphatdev <leatsophat02@gmail.com>
2025-01-19 22:41:05 +0700

    📦: Export Data

---
+++ src/lib/data.ts
@@ -0,0 +1,3 @@
+export const appName = "Leat Sophat"
+export const appPositions = ["Senior Front-end Developer.", "UI/UX Designer."]
+export const appDescriptions = "I worked at TurboTech with extensive experience in building modern web applications. Specializing in React, Next.js, and UI/UX design, Check out my portfolio to see my latest projects and design innovations."
