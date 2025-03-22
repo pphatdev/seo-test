7a46553f464fe097ebecdb33c8147f967f6a023b
pphatdev <leatsophat02@gmail.com>
2025-03-22 19:12:11 +0700

    Update Menu

--- src/components/dock-menu.tsx
+++ src/components/dock-menu.tsx
@@ -3,22 +3,31 @@
 import React from "react";
 
 import { Dock, DockIcon } from "@components/ui/dock";
-import { IconBrandCodecov, IconBrandFacebook, IconBrandFigma, IconBrandGithub, IconBrandTailwind } from "@tabler/icons-react";
+import { IconBrandBehance, IconBrandDribbble, IconBrandFigma, IconBrandGithub, IconBrandGitlab, IconBrandGoogle, IconBrandLinkedin } from "@tabler/icons-react";
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
-export function DockMenu() {
+export function NavMenu() {
     return (
         <div className="relative">
-            <Dock direction="middle">
-                <DockIcon>
+            <Dock direction="bottom">
+                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
                     <IconBrandGithub />
                 </DockIcon>
-                <DockIcon>
-                    <IconBrandFigma />
+                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
+                    <IconBrandGoogle />
                 </DockIcon>
-                <DockIcon>
-                    <IconBrandTailwind />
+                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
+                    <IconBrandLinkedin/>
+                </DockIcon>
+                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
+                    <IconBrandBehance/>
+                </DockIcon>
+                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
+                    <IconBrandDribbble/>
+                </DockIcon>
+                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
+                    <IconBrandFigma />
                 </DockIcon>
             </Dock>
         </div>
