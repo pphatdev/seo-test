cd07b55c04202d6463a66ad227005882c6a9f89b
pphatdev <leatsophat02@gmail.com>
2025-03-22 22:17:15 +0700

    Add Social Links

--- src/components/dock-menu.tsx
+++ src/components/dock-menu.tsx
@@ -3,32 +3,57 @@
 import React from "react";
 
 import { Dock, DockIcon } from "@components/ui/dock";
-import { IconBrandBehance, IconBrandDribbble, IconBrandFigma, IconBrandGithub, IconBrandGitlab, IconBrandGoogle, IconBrandLinkedin } from "@tabler/icons-react";
+import { IconBrandBehance, IconBrandDribbble, IconBrandFigma, IconBrandGithub, IconBrandGoogle, IconBrandLinkedin } from "@tabler/icons-react";
+import Link from "next/link";
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
+
+const items = [
+    {
+        name: "GitHub",
+        href: "https://github.com/pphatdev",
+        icons: IconBrandGithub
+    },
+    {
+        name: "Google Developer",
+        href: "https://g.dev/sophat",
+        icons: IconBrandGoogle
+    },
+    {
+        name: "Linked In",
+        href: "https://kh.linkedin.com/in/pphatdev",
+        icons: IconBrandLinkedin
+    },
+    // {
+    //     name: "Behance",
+    //     href: "",
+    //     icons: IconBrandBehance
+    // },
+    // {
+    //     name: "Dribbble",
+    //     href: "",
+    //     icons: IconBrandDribbble
+    // },
+    {
+        name: "Figma",
+        href: "https://www.figma.com/@PPhat",
+        icons: IconBrandFigma
+    },
+]
 
 export function NavMenu() {
     return (
         <div className="relative">
             <Dock direction="bottom">
-                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
-                    <IconBrandGithub />
-                </DockIcon>
-                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
-                    <IconBrandGoogle />
-                </DockIcon>
-                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
-                    <IconBrandLinkedin/>
-                </DockIcon>
-                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
-                    <IconBrandBehance/>
-                </DockIcon>
-                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
-                    <IconBrandDribbble/>
-                </DockIcon>
-                <DockIcon className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
-                    <IconBrandFigma />
-                </DockIcon>
+                {
+                    items.map( (item, key) => (
+                        <DockIcon key={key} className="from-primary text-white to-primary/50 rounded-xl bg-gradient-to-t inline-flex w-fit px-2 py-2 gap-1 border-border">
+                            <Link href={item.href} target="_blank" aria-label={item.name}>
+                                <item.icons />
+                            </Link>
+                        </DockIcon>
+                    ))
+                }
             </Dock>
         </div>
     );
