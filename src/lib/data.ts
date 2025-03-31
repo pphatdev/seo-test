384bbcb3bbf399d24909a8f92e0998e340f6b5df
PPhat DEv <pphat.kits@gmail.com>
2025-03-31 10:11:47 +0700

    Update appDescriptions for clarity and personal branding

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,3 @@
 export const appName = "Leat Sophat"
 export const appPositions = ["Senior Front-end Developer.", "UI/UX Designer."]
-export const appDescriptions = "I worked at TurboTech with extensive experience in building modern web applications. Specializing in React, Next.js, and UI/UX design, Check out my portfolio to see my latest projects and design innovations."
+export const appDescriptions = "Welcome to my portfolio - Thoughts from a wandering mind. Currently I work as a Senior Front-end Developer at TurboTech, and as a Freelancer UI/UX designer. In my free time, I enjoy designing side projects and developing them."
