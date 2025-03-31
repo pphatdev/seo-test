f77d3f753ebe74a131e4fd3c1d55e9df4de2aa9e
PPhat DEv <pphat.kits@gmail.com>
2025-03-31 13:27:09 +0700

    Add XML namespace and schema location to sitemap generation

--- src/scripts/generate-sitemap.ts
+++ src/scripts/generate-sitemap.ts
@@ -20,7 +20,12 @@ const routes: Route[] = [
 export function generateSitemap(): void {
     try {
         const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <urlset
+                xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+                <!-- created with pphat.netlify.app -->
                 ${routes.map(
                     route => (`
                         <url>
